47c88df6d73d42afe48f414a6905c4db8d63b11d
forceTlight <gamebox27@naver.com>
2022-05-09 13:59:16 +0900

    [INIT] Hobbydang Spring Project Initailizing

---
+++ src/main/java/com/neo/habidang/HabidangApplication.java
@@ -0,0 +1,13 @@
+package com.neo.habidang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HabidangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HabidangApplication.class, args);
+	}
+
+}
